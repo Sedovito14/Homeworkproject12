9f5599ffe7d73f60301edf9de15c1fbdb89891d3
Netotest <sakamotokenshin@yandex.ru>
2024-03-28 09:47:51 +0300

    Task completed

---
+++ src/main/java/netology/ru/NotFoundException.java
@@ -0,0 +1,6 @@
+package netology.ru;
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(int id) {
+        super("Товара с ID = " + id + " не существует");
+    }
+}
